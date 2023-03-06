ae5991df6f9a5b5867d41663c7df5ea2423cfd26
“vivek191290” <vivekpatil9525@gmail.com>
2023-03-06 23:00:08 +0530

    Add: As a simulator, loop through Multiple times of flipping a coin store the Singlet Combination in a Dictionary. Finally determine the percentage of the Singlet Combination. - Singlet shows Heads or Tails

--- flipCoinCombination.sh
+++ flipCoinCombination.sh
@@ -1,9 +1,29 @@
 echo "Welcome to Flip Coin Simulation prgram to winning percentage of Head or Tail"
 
-flip=$((RANDOM % 2))
+declare -A singletCombos
+total=0
 
-if [ $flip -eq 0 ]; then
-  echo "Heads"
-else
-  echo "Tails"
-fi
+echo "Simulating coin flips..."
+read -p "Enter the number of times to flip the coin: " numFlips
+
+for (( i=0; i<$numFlips; i++ ))
+do
+    flip=$(( RANDOM % 2 ))
+
+    if [ $flip -eq 0 ]
+    then
+        singletCombos[H]+=1
+    else
+        singletCombos[T]+=1
+    fi
+done
+
+echo "Results of coin flips:"
+
+for combo in "${!singletCombos[@]}"
+do
+    count=${#singletCombos[$combo]}
+    percentage=$(echo "scale=2; $count / $numFlips * 100" | bc)
+    echo "$combo: $count ($percentage%)"
+done
+
